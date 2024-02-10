8269b27172450fa115f4984cbbea79dcfefa3571
Gary Drake <gary_drake@fsafood.com>
2024-02-10 15:43:24 -0600

    icon

--- YoutubePlaylists/Form1.Designer.cs
+++ YoutubePlaylists/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace YoutubePlaylists
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.txtChannelId = new System.Windows.Forms.TextBox();
@@ -170,8 +171,9 @@ namespace YoutubePlaylists
             this.Controls.Add(this.txtChannelId);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Youtube Playlist Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
