ac7cdcc99073ae04e228cb1d5e242738837bd78d
Gary Drake <gary_drake@fsafood.com>
2024-02-12 00:48:52 -0600

    menu back color

--- YoutubePlaylists/Form1.Designer.cs
+++ YoutubePlaylists/Form1.Designer.cs
@@ -137,6 +137,7 @@ namespace YoutubePlaylists
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuBar;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem,
             this.exportingToolStripMenuItem,
